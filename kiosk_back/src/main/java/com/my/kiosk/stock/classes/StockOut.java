ed66f9e50f813ebdfc4131c8932cf6d339ee1196
choi wonjun <dnjswns5013@naver.com>
2025-03-27 10:41:50 +0900

    250327
    maven에서 gradle로 변경
    JPA 기능 제거

--- kiosk_back/src/main/java/com/my/kiosk/stock/classes/StockOut.java
+++ kiosk_back/src/main/java/com/my/kiosk/stock/classes/StockOut.java
@@ -2,30 +2,18 @@ package com.my.kiosk.stock.classes;
 
 import java.sql.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
-@Entity
 @Data
 public class StockOut {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private int id;
-	
-	@Column(nullable = false)
-	private int menu_id;
-	
-	@Column(nullable = false)
-	private int place_id;
-	
-	@Column(nullable = false)
+
+	private int menuId;
+
+	private int placeId;
+
 	private int amount;
-	
-	@Column(nullable = false)
+
 	private Date date;
 }
--- kiosk_back/src/test/java/com/my/kiosk/KioskBackApplicationTests.java
+++ kiosk_back/src/test/java/com/my/kiosk/KioskBackApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KioskApplicationTests {
+class KioskBackApplicationTests {
 
 	@Test
 	void contextLoads() {
