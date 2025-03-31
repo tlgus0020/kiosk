e0d5b06681c8260957eeb72209e074706b4eee87
choi wonjun <dnjswns5013@naver.com>
2025-03-31 15:37:34 +0900

    250331

---
+++ kiosk_admin_front/src/pages/Pay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pay = () => {
+    return (
+        <div>
+            <h1>결제 관리 페이지</h1>
+        </div>
+    );
+};
+
+export default Pay;
---
+++ kiosk_admin_front/src/pages/Stock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stock = () => {
+    return (
+        <div>
+            <h1>재고 관리 페이지</h1>
+        </div>
+    );
+};
+
+export default Stock;
